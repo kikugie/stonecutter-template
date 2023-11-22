90cc6925b409c6ac2fab0d7f008a1fc5c7a691b7
Kikugie <kikugie@duck.com>
2023-11-22 12:56:18 +0100

    Initial commit

---
+++ src/main/java/dev/kikugie/template/TemplateMod.java
@@ -0,0 +1,10 @@
+package dev.kikugie.template;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TemplateMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
